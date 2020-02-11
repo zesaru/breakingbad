49fca350dffad47eb8bf14da387f60546e3e9542
Cesar Murillo <namiayumis@gmail.com>
2020-02-11 19:15:06 +0900

    Boton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import React from "react";
+import styled from "@emotion/styled";
 
+const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Boton = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
 function App() {
-  return <h1>BreakingBad</h1>;
+  const consultarAPI = () => {
+    console.log("consultando.....");
+  };
+
+  return (
+    <Contenedor>
+      <Boton onClick={consultarAPI}>Obtener Frase</Boton>
+    </Contenedor>
+  );
 }
 
 export default App;
